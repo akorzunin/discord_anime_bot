7b12993bda0c90d6dd2d54f3c54de2e51801b70b
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2022-07-30 02:12:08 +0300

    debug

--- src/modules/Basic_commands.py
+++ src/modules/Basic_commands.py
@@ -5,20 +5,28 @@ import discord
 from datetime import datetime
 from time import sleep
 
+from modules.db_connector import db, memes
+
 from discord.ext import commands
 class Basic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.start_time = datetime.today()
+        self.start_time = datetime.now()
+
+    @commands.command(help='')
+    async def db_conn(self, ctx):
+        # time_to_response = round(self.bot.latency * 1000)
+        a = memes.insert(dict(name='1mem', pog=[1,2,3]))
+        await ctx.send(a)
 
     @commands.command(help='Shows time to response')
     async def ping(self, ctx):
         time_to_response = round(self.bot.latency * 1000)
-        await ctx.send(f'{datetime.today()} ping: {time_to_response}ms')
+        await ctx.send(f'{datetime.now()} ping: {time_to_response}ms')
 
     @commands.command(help='Shows uptime in format dddd:hh:mm:ss' ,)
     async def uptime(self, ctx):  # sourcery skip: square-identity
-        u = datetime.today() - self.start_time
+        u = datetime.now() - self.start_time
         upt = u.seconds
         days = upt//(60*60*24) # dddd
         hours = upt//(60*60) - days*24 # hh
